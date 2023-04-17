cf3978c465190558e91eb470fe5a0fa74c56757a
Ali Hassan Ali <69222334+Ali-Hassan-Ali@users.noreply.github.com>
2023-04-17 06:06:55 +0200

    add

--- resources/views/site/index/includes/about.blade.php
+++ resources/views/site/index/includes/about.blade.php
@@ -1,47 +1,37 @@
+@if(!empty(json_decode(getSetting('about_title'), true)))
 <section id="about" class="about">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 col-12">
-            <div class="section-headline text-center">
-              <h2>من نحن</h2>
-              <br>
-            </div>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-12">
+        <div class="section-headline text-center">
+          <h2>@lang('settings.about')</h2>
+          <br>
         </div>
-        <div class="row d-flex align-items-center">
-          <div class="col-md-5 col-12" data-aos="fade-up" data-aos-delay="100">
-            <div class="img-sec position-relative ">
-              <img src="assets/img/hero-carousel/02.jpg " class="img-fluid " height="200px" alt="">
-            </div>
-          </div>
-          <div class="col-md-7 col-12 text-right">
-            <h4 class="sec-head mt-4" data-aos="fade-up" data-aos-delay="200">فيول لقطع غيار السيارت</h4>
-            <p data-aos="fade-up" data-aos-delay="250">
-              شركة ذات مسؤولية محدودة (ذ) م م مقرها الرئيس مدينة جدة بالمملكة العربية السعودية لها فروع عدة في كل من الرياض، الدمام، خميس مشيط وتبوك. وهي شركة رائدة في مجال استيراد 
-وتصدير قطع غيار السيارات. 
-
-            </p>
-            <h4 class="sec-head mt-2" data-aos="fade-up" data-aos-delay="350">أولوياتنا </h4>
-
-            <p data-aos="fade-up" data-aos-delay="350">
-              من أولويات شركة فيول في السوق السعودي كسب ثقة عملائها مع التركيز على بقعة توسعها في السوق المحلي الإقليمي العربي وصولاً إلى الأسواق العالمية، هدفها شمول التخصص بمجالات 
-              مختلفة من قطع غيارات السيارات كالقطع الخارجية والشمعات 
-              والإضاءة وأنظمة الفرامل والتعليق وأنظمة التبريد للسيارة               
-            </p>
-            <p data-aos="fade-up" data-aos-delay="400">
-              وتعتبر فيول من الشركات المستوردة والمصدرة لماركات عالمية من 
-              قطع غيارات السيارات لمجموعة من الشركاء العالميين كالشركات اليابانية والكورية المعروفة عالمياً وفي السوق السعودي، بالإضافة الى زيادة نسبة الاستيراد من الشركات سابقة الذكر. 
-            </p>
-            <h4 class="sec-head mt-2" data-aos="fade-up" data-aos-delay="450">ما يميزنا</h4>
-
-            <p data-aos="fade-up" data-aos-delay="450">
-              تتميـز فـيـول بـأن جميع موظفيها ذوو خبرات عالية في مجال استيراد وتصدير قطع غيار السيارات وعلى دراية كاملة في السوق السعودي. 
-            </p>
-            </div>
-          </div>
-          <!-- End col-->
-        </div>
-
       </div>
+    </div>
+    <div class="row d-flex align-items-center">
+      <div class="col-md-5 col-12" data-aos="fade-up" data-aos-delay="100">
+        <div class="img-sec position-relative ">
+          <img src="{{ asset('storage/' . getSetting('about_image')) }}" class="img-fluid " height="200px" alt="">
+        </div>
+      </div>
+      <div class="col-md-7 col-12 text-right">
 
-    </section>
+        @foreach(json_decode(getSetting('about_title'), true) as $index=>$title)
+
+          <h4 class="sec-head mt-4" data-aos="fade-up" data-aos-delay="200">
+            {{ json_decode(getSetting('about_title'), true)[$index][app()->getLocale()] ?? '' }}
+          </h4>
+          <p data-aos="fade-up" data-aos-delay="250">
+            {!! json_decode(getSetting('about_description'), true)[$index][app()->getLocale()] ?? '' !!}
+          </p>
+
+        @endforeach
+      </div>
+      <!-- End col-->
+    </div>
+
+  </div>
+
+</section>
+@endif
--- resources/views/site/index/includes/facts.blade.php
+++ resources/views/site/index/includes/facts.blade.php
@@ -8,11 +8,11 @@
 
     <div class="row counters">
 
-      @foreach(json_decode(getSetting('achievement_name'), true) as $indexName=>$name)
+      @foreach(json_decode(getSetting('achievement_name'), true) as $index=>$name)
 
         <div class="col-lg-3 col-6 text-center">
-          <span data-purecounter-start="0" data-purecounter-end="{{ json_decode(getSetting('achievement_count'), true)[$indexName][app()->getLocale()] ?? '' }}" data-purecounter-duration="1" class="purecounter"></span>
-          <p>{{ json_decode(getSetting('achievement_name'), true)[$indexName][app()->getLocale()] ?? '' }}</p>
+          <span data-purecounter-start="0" data-purecounter-end="{{ json_decode(getSetting('achievement_count'), true)[$index][app()->getLocale()] ?? '' }}" data-purecounter-duration="1" class="purecounter"></span>
+          <p>{{ json_decode(getSetting('achievement_name'), true)[$index][app()->getLocale()] ?? '' }}</p>
         </div>
 
       @endforeach
